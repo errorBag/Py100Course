39f98a1abf4fed0867fac249e5f383ee6c7c6eab
dmibut <but9@mail.ru>
2024-04-03 20:13:12 +0300

    int

--- Функции и методы/Самостоятельная работа/task6/main.py
+++ Функции и методы/Самостоятельная работа/task6/main.py
@@ -2,17 +2,17 @@ ALLOW_SYMBOLS = ["0", "1"]  # Допустимые символы
 
 
 def check_string(str_):
-    if not str_:
-        return False
-    for i in set(str_):
-        if i not in ALLOW_SYMBOLS:
-            return False
-    return True
-    # if str_.isdigit() and (str_.replace('0', '') == str_.replace('1', '')):
-    #     return True
-    # else:
+    # if not str_:
     #     return False
-    # return str_.isdigit() and str_.replace('0', '') == str_.replace('1', '')
+    # for i in set(str_):
+    #     if i not in ALLOW_SYMBOLS:
+    #         return False
+    # return True
+    # if str_ and str_.isdigit():
+    if str_.isdigit() and str_.replace('0', '') == str_.replace('1', ''):
+        return True
+    else:
+        return False
 
 
 print(check_string("1010101010"))
