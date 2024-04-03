304be41512082d863dcd8eb0ee5ed06ef67e27cd
Дмитрий <dmitriibutorlin@gmail.com>
2024-04-03 20:23:36 +0300

    int1

--- Модули и пакеты. Работа с библиотеками/Исключения/Встроенные ошибки/main.py
+++ Модули и пакеты. Работа с библиотеками/Исключения/Встроенные ошибки/main.py
@@ -1,1 +1,8 @@
 # TODO Вызовите ошибку AssertionError с собственным текстом
+
+def summ(a = int, b = int):
+    return a + b
+    if a not a.isdigit():
+        raise AssertionError("Текст ошибки")
+
+print(summ(3, 4))
